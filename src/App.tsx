bef534376cf0d17c6d0575ef59348f9868d5f236
Gohikeco1 <gohikeco1@gmail.com>
2021-03-02 22:18:24 -0600

    adds useState hook to manage user input

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [userInput, setUserInput] = useState("");
+	const [reverseOutput, setReverseOutput] = useState("");
+
+	const onUserInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const value = e.currentTarget.value;
+		setUserInput(value);
+		setReverseOutput(value.split("").reverse().join(""));
+	};
+
+	return (
+		<Layout>
+			<form>
+				<StyledInput value={userInput} onChange={onUserInputChange} />
+			</form>
+			<div>{reverseOutput}</div>
+		</Layout>
+	);
+};
 
 export default App;
+
+const Layout: React.FC = ({ children }) => {
+	return (
+		<div
+			sx={{
+				height: "100vh",
+				width: "100vw",
+				background: "lightgray",
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+				flexDirection: "column",
+			}}
+		>
+			{children}
+		</div>
+	);
+};
+
+const StyledInput: React.FC<{
+	value: string;
+	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}> = ({ value, onChange }) => {
+	return (
+		<input
+			sx={{ boxShadow: "10px 5px 5px #0004" }}
+			value={value}
+			onChange={onChange}
+		/>
+	);
+};
